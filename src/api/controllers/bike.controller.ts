3a9b7c4e47bd81d5c6fd24dcb5d0b6c17d5571bf
yogesh <kathayat.yogesh@gmail.com>
2019-03-22 17:51:53 +0400

    validation check and test cases added

--- src/api/controllers/bike.controller.ts
+++ src/api/controllers/bike.controller.ts
@@ -1,8 +1,14 @@
 import { Request, Response } from "express";
-
+import { check, validationResult } from "express-validator/check";
 class BikeController {
     public async addBike(req: Request, res: Response) {
         try {
+
+            const errors = await validationResult(req);
+            console.log(errors.array(), errors.isEmpty());
+            if (!errors.isEmpty()) {
+              return res.status(422).json({ errors: errors.array() });
+          }
             console.log(req.body);
 
 
